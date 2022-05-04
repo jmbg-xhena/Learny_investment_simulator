9d51a779c3b9a538c2e1970caa07b41f0ee0e9ee
José María Beristain <dedocebabotas@hotmail.com>
2022-05-04 15:15:11 -0500

    Datos random de industrias completos

--- Investment_simulator/Assets/Simulator/Systems/SimulatorData/Scripts/SimulatorRandomData.cs
+++ Investment_simulator/Assets/Simulator/Systems/SimulatorData/Scripts/SimulatorRandomData.cs
@@ -37,9 +37,14 @@ public class SimulatorRandomData : MonoBehaviour
     [SerializeField] private float tec_opportunityCostNonViable_min;
     [SerializeField] private float tec_opportunityCostNonViable_max;
     public float tec_annualCashFlow;//calcular con las otras variables
+    [SerializeField] private float tec_annualCashFlowRate1 = 2.62f;
+    [SerializeField] private float tec_annualCashFlowRate2 = 3.39f;
+    [SerializeField] private float tec_annualCashFlowRate3 = 4.1f;
     public float tec_annualPersonnelExpenses; //calcular con las otras variables
     public float tec_annualAdvertisingExpenses; //calcular con las otras variables
     public float tec_annualSales; //calcular con las otras variables
+    public float tec_min_tir = 6;
+    public float tec_max_tir = 8;
     
     [Header("Real estate")]
     public int rS_initialInvest;
@@ -54,9 +59,14 @@ public class SimulatorRandomData : MonoBehaviour
     [SerializeField] private int rS_salePrice_min;
     [SerializeField] private int rS_salePrice_max;
     public float rS_annualCashFlow;//calcular con las otras variables
+    [SerializeField] private float rS_annualCashFlowRate1 = 2.4f;
+    [SerializeField] private float rS_annualCashFlowRate2 = 3.04f;
+    [SerializeField] private float rS_annualCashFlowRate3 = 3.6f;
     public float rS_annualMaintenanceExpenses;//calcular con las otras variables
     public float rS_annualTaxExpense;//calcular con las otras variables
     public float rS_AnnualLease;//calcular con las otras variables
+    public float rS_min_tir = 11;
+    public float rS_max_tir = 13;
 
     [Header("Manufacturing")]
     public int manu_initialInvest;
@@ -71,10 +81,15 @@ public class SimulatorRandomData : MonoBehaviour
     [SerializeField] private int manu_unitsProducedAnnually_min;
     [SerializeField] private int manu_unitsProducedAnnually_max;
     public float manu_annualCashFlow;//calcular con las otras variables
+    [SerializeField] private float manu_annualCashFlowRate1 = 2.58f;
+    [SerializeField] private float manu_annualCashFlowRate2 = 3.31f;
+    [SerializeField] private float manu_annualCashFlowRate3 = 3.99f;
     public float manu_unitCostProduction;//calcular con las otras variables
     public float manu_annualOperatingExpenses;//calcular con las otras variables
     public float manu_annualAdvertisingExpenses;//calcular con las otras variables
     public float manu_unitSellingPrice;//calcular con las otras variables
+    public float manu_min_tir = 7;
+    public float manu_max_tir = 9;
 
     [Header("Financial")]
     public int fin_initialInvest;
@@ -85,7 +100,13 @@ public class SimulatorRandomData : MonoBehaviour
     [SerializeField] private float fin_opportunityCostViable_max;
     [SerializeField] private float fin_opportunityCostNonViable_min;
     [SerializeField] private float fin_opportunityCostNonViable_max;
-    public float fin_annualProduction;//calcular con las otras variables
+    public float fin_annualProduction;
+    public float fin_annualProductionRate1 = 2.53f;
+    public float fin_annualProductionRate2 = 3.24f;
+    public float fin_annualProductionRate3 = 3.89f;
+    public float fin_min_tir = 8;
+    public float fin_max_tir = 10;
+
 
     public static SimulatorRandomData instance;
 
@@ -123,18 +144,46 @@ public class SimulatorRandomData : MonoBehaviour
 
         //Technology
         tec_opportunityCost = SetOportunityCost(Projects.Technology, tec_opportunityCostViable_min, tec_opportunityCostViable_max, tec_opportunityCostNonViable_min, tec_opportunityCostNonViable_max);
+        tec_annualCashFlow = GetAnnualCashFLow(tec_initialInvest, tec_annualCashFlowRate1, tec_annualCashFlowRate2, tec_annualCashFlowRate3);
+        tec_annualPersonnelExpenses = Mathf.Ceil(tec_annualCashFlow * 3 * 0.75f);
+        tec_annualAdvertisingExpenses = (tec_annualCashFlow * 3) - tec_annualPersonnelExpenses;
+        tec_annualSales = tec_annualCashFlow * 4;
 
         //Real estate
         rS_opportunityCost = SetOportunityCost(Projects.RealEstate, rS_opportunityCostViable_min, rS_opportunityCostViable_max, rS_opportunityCostNonViable_min, rS_opportunityCostNonViable_max);
         rS_salePrice = Random.Range(rS_salePrice_min, rS_salePrice_max + 1);
+        rS_annualCashFlow = GetAnnualCashFLow(rS_initialInvest - (rS_salePrice / Mathf.Pow(1.12f, expectedProfitTime)), rS_annualCashFlowRate1, rS_annualCashFlowRate2, rS_annualCashFlowRate3);
+        rS_annualMaintenanceExpenses = (rS_annualCashFlow / 6.7f) * 0.33f;
+        rS_annualTaxExpense = (rS_annualCashFlow / 6.7f) - rS_annualMaintenanceExpenses;
+        rS_AnnualLease = rS_annualCashFlow + rS_annualMaintenanceExpenses + rS_annualTaxExpense;
 
         //Manufacturing
         manu_opportunityCost = SetOportunityCost(Projects.Manufacturing, manu_opportunityCostViable_min, manu_opportunityCostViable_max, manu_opportunityCostNonViable_min, manu_opportunityCostNonViable_max);
         manu_unitsProducedAnnually = Random.Range(manu_unitsProducedAnnually_min, manu_unitsProducedAnnually_max + 1);
+        manu_annualCashFlow = GetAnnualCashFLow(manu_initialInvest, manu_annualCashFlowRate1, manu_annualCashFlowRate2, manu_annualCashFlowRate3);
+        manu_unitCostProduction = Mathf.Ceil((manu_annualCashFlow / (manu_unitsProducedAnnually * 0.4f)) * 0.8f);
+        manu_annualOperatingExpenses = Mathf.Ceil((manu_annualCashFlow / 0.4f) * 0.2f * 0.6f);
+        manu_annualAdvertisingExpenses = Mathf.Ceil((manu_annualCashFlow / 0.4f * 0.2f) - manu_annualOperatingExpenses);
+        manu_unitSellingPrice = (manu_annualCashFlow + manu_unitsProducedAnnually * manu_unitCostProduction + manu_annualOperatingExpenses + manu_annualAdvertisingExpenses) / manu_unitsProducedAnnually;
 
         //Financial
         fin_opportunityCost = SetOportunityCost(Projects.Financial, fin_opportunityCostViable_min, fin_opportunityCostViable_max, fin_opportunityCostNonViable_min, fin_opportunityCostNonViable_max);
         fin_annualProduction = GetFinAnnualProduction();
+    }
+
+    float GetAnnualCashFLow(float InitialInvest, float cashFlowRate1, float cashFlowRate2, float cashFlowRate3) {
+        if (expectedProfitTime == 3) {
+            return Mathf.Ceil(InitialInvest / cashFlowRate1);
+        }
+        if (expectedProfitTime == 4)
+        {
+            return Mathf.Ceil(InitialInvest / cashFlowRate2);
+        }
+        if (expectedProfitTime == 5)
+        {
+            return Mathf.Ceil(InitialInvest / cashFlowRate3);
+        }
+        return -1;
     }
 
     float  SetOportunityCost(Projects proj, float opportunityCostViable_min, float opportunityCostViable_max, float opportunityCostNonViable_min, float opportunityCostNonViable_max) {
@@ -189,15 +238,15 @@ public class SimulatorRandomData : MonoBehaviour
 
     float GetFinAnnualProduction() {
         if (expectedProfitTime == 3) {
-            return (fin_initialInvest / 2.53f);
+            return (fin_initialInvest / fin_annualProductionRate1);
         }
         if (expectedProfitTime == 4)
         {
-            return (fin_initialInvest / 3.24f);
+            return (fin_initialInvest / fin_annualProductionRate2);
         }
         if (expectedProfitTime == 5)
         {
-            return (fin_initialInvest / 3.89f);
+            return (fin_initialInvest / fin_annualProductionRate3);
         }
         return -1;
     }
